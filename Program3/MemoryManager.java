78be36392159fad9b637974af26bcef8f0726682
elmehdiziate <mziate23@gmail.com>
2023-03-30 22:35:44 -0500

    add blocks initial

--- Program3/MemoryManager.java
+++ Program3/MemoryManager.java
@@ -1,4 +1,3 @@
-package Program3;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,7 +16,7 @@ class MemoryManager{
         private boolean free;
 
         public Block(int start,int size){
-            // Assumption that ID= -1 means that the block is free
+            // Assumption ID= -1 means that the block is free
             this.id = -1;
             this.start = start; 
             this.size = size;
@@ -63,4 +62,106 @@ class MemoryManager{
         }
     }
 
+    public MemoryManager(int size) {
+        memorySize = size;
+        memoryBlocks = new ArrayList<Block>();
+        memoryBlocks.add(new Block(1 , size));
+    }
+
+    public void run(){
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("Enter command (a/dd, d/isplay, f/ree, q/uit):");
+            String command = sc.next();
+
+            if(command.equals("a") || command.equals("dd")){
+                int id = sc.nextInt();
+                int size = sc.nextInt();
+                String Method = sc.next();
+                System.out.println(Method + size + id);
+                addBlock(id, size, Method);
+            }
+            else if(command.equals("d") || command.equals("isplay")){
+                displayBlocks();
+            }
+            else if(command.equals("f") || command.equals("ree")){
+                int id = sc.nextInt();
+                FreeBlocks(id);
+            }
+            else if(command.equals("q") || command.equals("uit")){
+                sc.close();
+                return;
+            }
+            else{
+                System.out.println("The command entered is not available !!!");
+            }
+        }
+    }
+
+    public static void main(String args[]){
+        int memsize = Integer.parseInt(args[0]);
+
+        MemoryManager M = new MemoryManager(memsize);
+
+        M.run();
+    }
+
+    private void addBlock(int id, int size, String Method){
+        int index = -1;
+        int start = -1;
+
+        if(Method.equals("f")){
+            int i = 0;
+            for(Block b : memoryBlocks){
+                if(b.isFree() && b.getSize() > size){
+                    start = b.getStart();
+                    index = i;
+                    break;
+                }
+                i++;
+            }
+        }
+        else if(Method.equals("b")){
+            int bestfit = Integer.MAX_VALUE;
+            int i = 0;
+            for(Block b : memoryBlocks){
+                if(b.isFree() && b.getSize() > size){
+                    int fit = b.getSize() - size;
+                    if(bestfit > fit){
+                        bestfit = fit;
+                        index = i;
+                    }
+                }
+                i++;
+            }
+        }
+        else if (Method.equals("w")){
+            int worstfit = Integer.MAX_VALUE;
+            int i = 0;
+            for(Block b : memoryBlocks){
+                if(b.isFree() && b.getSize()> size){
+                    int fit = b.getSize() - size;
+                    if(fit > worstfit){
+                        worstfit = fit;
+                        index = i;
+                    }
+                }
+                i++;
+            }
+        }
+
+        if(index != -1){
+            memoryBlocks.add(index , new Block(start, size));
+            memoryBlocks.get(index).setId(id);
+        }
+    }
+
+
+    private void displayBlocks(){
+
+    }
+
+    private void FreeBlocks(int id){
+
+    }
 }
