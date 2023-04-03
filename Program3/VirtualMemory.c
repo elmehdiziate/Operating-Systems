ca8f335cb6c47a930ad5450a26c36f843290dc2a
elmehdiziate <mziate23@gmail.com>
2023-04-03 18:35:02 -0500

    FIFO for frames+1 and frames+2/ testing

--- Program3/VirtualMemory.c
+++ Program3/VirtualMemory.c
@@ -15,6 +15,7 @@ int * generate_reference_string(int range, int accesses){
 
 int fifo(int* reference_string, int accesses, int num_frames){
 
+    printf("this is the number of frames: %d\n",num_frames);
     // initialize memory and page table
     int *memory = (int *) calloc(num_frames, sizeof(int));
     bool *page_table = (bool *) calloc(accesses, sizeof(bool));
@@ -61,8 +62,8 @@ int main(int argc, char *argv[]){
 
     int* reference_string;
     int FIFO_total_faults = 0, FIFO_min_faults = 0, FIFO_max_faults = 0;
-    int FIFO_plus_one_total_faults = 0, FIFO_plus_one_min_faults = 0, FIFO_plus_one_max_faults = 0, FIFO_plus_one_belady_count = 0;
-    int FIFO_plus_two_total_faults = 0, FIFO_plus_two_min_faults = 0, FIFO_plus_two_max_faults = 0, FIFO_plus_two_belady_count = 0;
+    int FIFO_plus_one_total_faults = 0, FIFO_plus_one_min_faults = NUM_ACCESSES, FIFO_plus_one_max_faults = 0, FIFO_plus_one_belady_count = 0;
+    int FIFO_plus_two_total_faults = 0, FIFO_plus_two_min_faults = NUM_ACCESSES, FIFO_plus_two_max_faults = 0, FIFO_plus_two_belady_count = 0;
     // generate a random reference string
     for(int i=0; i<NUM_ITERATIONS; i++ ){
         reference_string = generate_reference_string(x, NUM_ACCESSES);
@@ -70,13 +71,53 @@ int main(int argc, char *argv[]){
             printf("%d-%d\t",i+1,reference_string[i]);
         }
         printf("\n");
+
+        // run fifo for number of frames
         int fifo_faults = fifo(reference_string, NUM_ACCESSES, f);
-        printf("%d",fifo_faults);
-        
         FIFO_total_faults += fifo_faults;
-        printf("\n");
-        printf("%d",FIFO_total_faults);
+        if(FIFO_min_faults > fifo_faults){
+            FIFO_min_faults = fifo_faults;
+        }
+        if(FIFO_max_faults < fifo_faults){
+            FIFO_max_faults = fifo_faults;
+        }
+
+        // run fifo for number of frames + 1 
+        int fifo_faults_1 = fifo(reference_string, NUM_ACCESSES, f+1);
+        FIFO_plus_one_total_faults += fifo_faults_1;
+        if(FIFO_plus_one_min_faults > fifo_faults_1){
+            FIFO_plus_one_min_faults = fifo_faults_1;
+        }
+        if(FIFO_plus_one_max_faults < fifo_faults_1){
+            FIFO_plus_one_min_faults = fifo_faults_1;
+        }
+
+        // check for Belady's Anomaly
+        printf("%d vs %d\n",fifo_faults,fifo_faults_1);
+        if(fifo_faults < fifo_faults_1){
+            FIFO_plus_one_belady_count++;
+        }
+
+        // run fifo for number of frames + 2
+        int fifo_faults_2 = fifo(reference_string, NUM_ACCESSES, f+1);
+        FIFO_plus_one_total_faults += fifo_faults_2;
+        if(FIFO_plus_two_min_faults > fifo_faults_2){
+            FIFO_plus_two_min_faults = fifo_faults_2;
+        }
+        if(FIFO_plus_two_max_faults < fifo_faults_1){
+            FIFO_plus_two_max_faults = fifo_faults_2;
+        }
+
+        // check for Belady's Anomaly
+        printf("%d vs %d\n",fifo_faults,fifo_faults_2);
+        if(fifo_faults < fifo_faults_2){
+            FIFO_plus_two_belady_count++;
+        }
+
+        
+
     }
-    
+    printf("%d\n",FIFO_plus_one_belady_count);
+    printf("%d\n",FIFO_plus_two_belady_count);
 
 }
