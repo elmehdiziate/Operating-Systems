4bb03e2eed9cf8b8d1988478230a9b58b2ed2e1d
elmehdiziate <mziate23@gmail.com>
2023-04-21 22:44:03 -0500

    Program mostly done - need comments

--- Program4/AsynchIO.java
+++ Program4/AsynchIO.java
@@ -32,10 +32,72 @@ public class AsynchIO {
         try(BufferedReader reader = Files.newBufferedReader(inputPath, StandardCharsets.US_ASCII)){
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                String[] parts = line.split(" ");
+                if(parts.length != 4){
+                    logger.severe("Parsing input file failure.");
+                    System.exit(1);
+                }
+                int opid = Integer.parseInt(parts[0]);
+                String filePath = parts[1];
+                int byteOffset = Integer.parseInt(parts[2]);
+                int byteCount = Integer.parseInt(parts[3]);
+
+                if(opid < 0){
+                    logger.warning("Invalid read operation: operation ID.");
+                    continue;
+                }
+
+                if (byteCount < 0 || byteOffset < 0){
+                    logger.warning("Invalid read operation: bad byte offset or count.");
+                    continue;
+                }
+
+                try{
+                    AsynchronousFileChannel afc = AsynchronousFileChannel.open(Paths.get(filePath), StandardOpenOption.READ);
+                    ByteBuffer buffer = ByteBuffer.allocate(byteCount);
+
+                    afc.read(buffer, byteOffset, null, new CompletionHandler<Integer, Void>() {
+                        @Override
+                        public void completed(Integer bytesRead, Void attachment) {
+                            if (bytesRead < 0) {
+                                logger.warning("Specified portion of the file does not exist.");
+                                return;
+                            }
+                            buffer.flip();
+                            String data = StandardCharsets.US_ASCII.decode(buffer).toString();
+                            System.out.println(opid + " " + data);
+                            try {
+                                afc.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void failed(Throwable exc, Void attachment) {
+                            logger.severe("Asynchronous read failed: " + exc.getMessage());
+                            try {
+                                afc.close();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }catch(IOException e){
+                    System.out.println(inputPath + "\tdoes not exist");
+                    e.printStackTrace();
+                    System.exit(1);
+                }
             }
         }catch(IOException e){
+            e.printStackTrace();
+            System.exit(1);
+        }
 
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
     }
